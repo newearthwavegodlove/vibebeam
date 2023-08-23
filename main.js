2534c382338ab8406dab01861f4aca76b5c4dcc9
tommyscience <tommydatbo@gmail.com>
2023-08-23 09:28:44 -0700

    Added rainbow hover effect, video wave, music intro,  and other updates

---
+++ main.js
@@ -0,0 +1,6 @@
+function playAudio() {
+    var audio = document.getElementById("myAudio");
+    audio.play();
+    document.getElementById("playButton").style.display = "none"; // Optionally, hide the play button after clicking
+  }
+  
